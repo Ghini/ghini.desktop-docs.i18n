20d9ef6a8b5a816f661392e7dbb3e4279aa635cb
Mario Frasca <mario@anche.no>
2017-06-24 17:45:40 -0500

    script scans single po file completing missing translations using translated.net

---
+++ test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+import codecs
+import json
+import requests
+
+def translate(s):
+    r = requests.get('http://api.mymemory.translated.net/get?q=%s&langpair=en|es' % s, timeout=6)
+    j = json.loads(r.text)
+    return j['responseData']['translatedText']
+
+with codecs.open("po/es.po") as f:
+    defining = None
+    msgdef = {'msgid': '', 'msgstr': ''}
+    complete = {'msgid': '', 'msgstr': ''}
+    lines = f.readlines()
+    for n, l in enumerate(lines):
+        l = l.strip()
+        if not l:
+            defining = None
+            if not msgdef['msgstr'] and msgdef['msgid'].find(' ') != -1:
+                msgdef['msgstr'] = translate(msgdef['msgid'])
+                complete['msgstr'] = u'msgstr "%s"\n' % msgdef['msgstr']
+            print '%(msgid)s%(msgstr)s' % complete
+            continue
+        if l[0] == '#':
+            print l
+            continue
+        if l.startswith('msgid'):
+            msgdef = {'msgid': '', 'msgstr': ''}
+            complete = {'msgid': '', 'msgstr': ''}
+            defining = 'msgid'
+            complete[defining] += l + '\n'
+            l = l[6:]
+            msgdef[defining] += l[1:-1]
+            continue
+        if l.startswith('msgstr'):
+            defining = 'msgstr'
+            complete[defining] += l + '\n'
+            l = l[7:]
+            msgdef[defining] += l[1:-1]
+            continue
+        if defining:
+            msgdef[defining] += l[1:-1]
+            complete[defining] += l + '\n'
+print 'msgid "%(msgid)s"\nmsgstr "%(msgstr)s"\n' % msgdef
