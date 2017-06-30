91c72d1037c013dd545b45ce63ae282f74a2bb25
Mario Frasca <mario@anche.no>
2017-06-29 20:26:34 -0500

    ignoring strings from a given list

--- doc/runme.sh
+++ doc/runme.sh
@@ -20,14 +20,14 @@ echo "done copying/updating files from documentation"
 echo '--------------------------------------------------------------------------'
 echo
 echo '=========================================================================='
-echo "update the centralised doc.pot (two steps, first all pot, then merge them)"
+echo "update the centralised doc.pot (prepare all pot, merge them, filter)"
 echo '--------------------------------------------------------------------------'
 echo "update the centralised doc.pot --- step one"
 echo '--------------------------------------------------------------------------'
 make gettext
-echo "update the centralised doc.pot --- step two"
+echo "update the centralised doc.pot --- step two and three"
 echo '--------------------------------------------------------------------------'
-msgcat -o _build/locale-merged/doc.pot _build/locale/*.pot
+msgcat _build/locale/*.pot | msggrep --msgid --file not_to_be_translated.txt --invert-match -o _build/locale-merged/doc.pot
 echo "done updating centralised doc.pot"
 echo '--------------------------------------------------------------------------'
 echo
